a66758d76f60a263d7a7a9739c165b7e2cf8e4bd
taitranhuu2302 <tai.tranhuu2002@gmail.com>
2023-05-16 19:26:52 +0700

    fix js drawer

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,50 +1,50 @@
-const btnDrawerAuthor = document.getElementsByClassName("mgi_header__btn-drawer-right")[0];
-const btnDrawerNav = document.getElementById("mgi_btn-nav-mobile");
-const btnCloseDrawerAuthor = document.getElementsByClassName("mgi_drawer__btn-close")[0];
-const drawerAuthor = document.getElementById("mgi_drawer-author");
-const drawerNav = document.getElementById("mgi_drawer-nav");
 const app = document.getElementById("app");
-const overlay = document.getElementById("overlay");
+const drawerAuthor = document.getElementById("mgi_drawer-author");
+const btnDrawerAuthor = document.getElementsByClassName(
+  "mgi_header__btn-drawer-right"
+)[0];
+const btnCloseDrawerAuthor = document.getElementsByClassName(
+  "mgi_drawer__btn-close"
+)[0];
+const drawerNavMobile = document.getElementById("mgi_drawer-nav")
+const btnDrawerNavMobile = document.getElementById("mgi_btn-nav-mobile");
+const overlay = document.getElementById("overlay")
 
-function toggleDrawerAuthor() {
-    if (drawerAuthor.classList.contains('open')) {
-        closeDrawerAuthor()
-    } else {
-        openDrawerAuthor()
+function openDrawer(target, size, type) {
+  if (type === "open") {
+    target.classList.add("open");
+    overlay.classList.add("open")
+  } else if (type === "close") {
+    target.classList.remove("open");
+    overlay.classList.remove("open")
+  }
+  app.style.transform = `translateX(${size})`;
+}
+
+btnDrawerAuthor.addEventListener("click", () => {
+  if (drawerAuthor.classList.contains("open")) {
+    openDrawer(drawerAuthor, 0, "close");
+  } else {
+    drawerAuthor.classList.add("open");
+    openDrawer(drawerAuthor, `-350px`, "open");
+  }
+});
+
+btnCloseDrawerAuthor.addEventListener("click", () => {
+    openDrawer(drawerAuthor, 0, "close");
+});
+
+btnDrawerNavMobile.addEventListener("click", () => {
+    openDrawer(drawerNavMobile, 0, "open");
+})
+
+document.addEventListener("click", (e) => {
+    if (e.target === overlay) {
+        if (drawerAuthor.classList.contains("open")) {
+            openDrawer(drawerAuthor, 0, "close");
+        }
+        if (drawerNavMobile.classList.contains("open")) {
+            openDrawer(drawerNavMobile, 0, "close");
+        }
     }
-}
-function openDrawerAuthor() {
-    drawerAuthor.classList.remove('open')
-    app.style.transform = `translateX(0)`
-    // overlay.classList.add('open')
-}
-
-function closeDrawerAuthor() {
-    app.style.transform = `translateX(-350px)`
-    drawerAuthor.classList.remove('open')
-}
-
-btnDrawerAuthor.addEventListener("click", (e) => {
-    toggleDrawerAuthor()
 })
-btnCloseDrawerAuthor.addEventListener("click", (e) => {
-    toggleDrawerAuthor()
-})
-
-// btnDrawerNav.addEventListener('click', () => {
-//     if (drawerNav.classList.contains("open")) {
-//         drawerNav.classList.remove("open")
-//     } else {
-//         drawerNav.classList.add("open")
-//         overlay.classList.add('open')
-//     }
-// })
-
-// document.addEventListener('click', (e) => {
-//     if (e.target.getAttribute('id') === 'overlay') {
-//         if (drawerAuthor.classList.contains('open')) {
-//             closeDrawerAuthor()
-//         }
-//         overlay.classList.add('open')
-//     }
-// })
